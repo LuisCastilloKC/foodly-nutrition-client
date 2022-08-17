f57d2252fd7281a0289197d0132fc587f7e01316
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-16 21:53:48 -0400

    add login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,27 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = http://localhost:3000/
+const API_URL = "http://localhost:3000/";
 
 const register = (username, email, password) => {
-    return axios.post(API_URL + "nutritionists", {
-        username,
-        email,
-        password
-    })
-}
+  return axios.post(API_URL + "nutritionists", {
+    username,
+    email,
+    password,
+  });
+};
 
-export default {register}
+const login = (username, password) => {
+  return axios.post(API_URL + "login", {
+    username,
+    password
+  })
+  .then((response) => {
+    console.log(response)
+    if (response.data.token) {
+      localStorage.setItem("nutritionist", JSON.stringify(response.data))
+    }
+    return response.data
+  })
+};
+
+export default { register, login };
