8286c6d872b330ca4b87a09ab56a2d52b332c127
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-22 22:14:56 -0400

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Login from './components/user/Login';
-import Registration from './components/user/Registration';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./components/user/Login";
+import Registration from "./components/user/Registration";
 
 function App() {
   return (
-    <div>
-      <Registration />
-      <Login />
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
